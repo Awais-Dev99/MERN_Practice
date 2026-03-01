d6c6f512009f7e847233205a502f9bb45b51ae5d
Awais-Dev99 <pakstd449@gmail.com>
2026-03-01 11:48:43 +0500

    update

--- Javascript 5/js5.js
+++ Javascript 5/js5.js
@@ -95,3 +95,6 @@ console.log(data);
 
 
 
+
+
+
--- Todo Practice (Incomplete)/todo.js
+++ Todo Practice (Incomplete)/todo.js
@@ -14,30 +14,71 @@
 
 
 
-const box=document.getElementById("todo-input")
-const list=document.getElementById("todo-list")
+// const box=document.getElementById("todo-input")
+// const list=document.getElementById("todo-list")
 
+// function add(){
+//     if(box.value === ''){
+//         alert("You must write something")
+//     }
+//     else{
+//         let li=document.createElement("li");
+//         li.innerHTML=box.value;
+//         list.appendChild(li)
+//         let span=document.createElement("span");
+//         span.innerHTML="\u00d7";
+//         li.appendChild(span);
+
+//     }
+// box.value='';
+// }
+// list.addEventListener("click",function(e){
+//     if(e.target.tagName==="Li"){
+//         e.target.classList.toggle("checked");
+//     }
+//     else if (e.target.tagName==="SPAN"){
+//         e.target.parentElement.remove();
+//     }
+// },false)
+
+
+
+
+const box=document.getElementById("todo-input");
+const list=document.getElementById("todo-list");
 function add(){
-    if(box.value === ''){
-        alert("You must write something")
-    }
-    else{
-        let li=document.createElement("li");
-        li.innerHTML=box.value;
-        list.appendChild(li)
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);
-
-    }
-box.value='';
+if(box.value===""){
+    alert("You must write something");
 }
+else {
+    let li=document.createElement("li");
+    li.innerHTML=box.value;
+    list.appendChild(li);
+    let span=document.createElement("span")
+    span.innerHTML="\u00d7";
+    li.appendChild(span)
+}box.value="";
+}
+
 list.addEventListener("click",function(e){
-    if(e.target.target==="Li"){
-        e.target.classList.toggle("checked");
+    if(e.target.tagName==="LI"){
+        e.target.classList.toogle("checked");
     }
-    else if (e.target.tagName="span"){
+    else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove();
     }
-},false)
+},false);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
