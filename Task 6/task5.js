08371fdb5ab9c047a6e189e8b25200ebd7b4a4fa
Awais-Dev99 <pakstd449@gmail.com>
2026-03-02 22:41:20 +0500

    update

---
+++ Task 6/task5.js
@@ -0,0 +1,26 @@
+let imag=document.getElementById("img");
+let sho=document.getElementById("showBtn");
+let hide=document.getElementById("hideBtn");
+// function show(){
+// imag.style.display="inline";
+// hide.style.display="inline";
+// sho.style.display="none";
+
+// }
+// function hidde(){
+//     imag.style.display="none";
+//     hide.style.display="none";
+
+// sho.style.display="inline";
+// }
+
+function show(){
+    imag.style.display="inline";
+    sho.style.display="none";
+    hide.style.display="inline";
+}
+function hidde(){
+    imag.style.display="none";
+    sho.style.display="inline";
+    hide.style.display="none";
+}
