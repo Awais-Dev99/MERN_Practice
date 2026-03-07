0b7da9d77ae94d31d96e22d5887fd1ed31d4810e
Awais-Dev99 <pakstd449@gmail.com>
2026-03-07 15:38:18 +0500

    update

---
+++ User Management App/tod.js
@@ -0,0 +1,35 @@
+let tableBody = document.getElementById("userTable");
+let form = document.getElementById("userForm");
+let id = 1;
+
+form.addEventListener("submit", addbtn);
+
+function addbtn(event) {
+
+event.preventDefault(); // stop page reload
+
+let first = document.getElementById("firstName").value;
+let last = document.getElementById("lastName").value;
+let email = document.getElementById("email").value;
+let phone = document.getElementById("phone").value;
+
+if(first === "" || last === "" || email === "" || phone === ""){
+    alert("Please fill all fields");
+    return;
+}
+
+let tr = document.createElement("tr");
+
+tr.innerHTML = `
+<td class="border p-2">${id++}</td>
+<td class="border p-2">${first}</td>
+<td class="border p-2">${last}</td>
+<td class="border p-2">${email}</td>
+<td class="border p-2">${phone}</td>
+<td class="border p-2 text-red-500 cursor-pointer" onclick="this.parentElement.remove()">Delete</td>
+`;
+
+tableBody.appendChild(tr);
+
+form.reset();
+}
